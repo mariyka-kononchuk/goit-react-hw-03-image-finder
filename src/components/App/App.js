52f357aa708495966c47cb2c0c58949edfab8afb
Maria Kononchuk <77247240+mariyka-kononchuk@users.noreply.github.com>
2021-09-13 15:39:11 +0300

    Loader cimponent created

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -24,68 +24,7 @@ class App extends Component {
     images: null,
     filter: '',
     showModal: false,
-    loading:false
   }
-
-  // async componentDidMount() {
-  //   this.setState({ loading: true });
-  //   let queryParams = `?key=${apiKey}&q=${this.state.searchName}&image_type=photo&per_page=12&page=1&orientation=horizontal&safesearch=true`;
-  //   let url = baseUrl + queryParams;
-  //   const response = await axios.get(url);
-  //   const images = response.data.hits;
-  //   console.log(images);
-  //   // let id = images.id;
-  //   // console.log(id);
-  //   // let webformatURL = images.previewURL;
-  //   // let largeImageURL = images.pageURL;
-  //   // записываем в state полученные картинки
-  //   this.setState({ images, loading: false });
-  //   // this.setState({ loading: false });
-  //   }
-
-    componentDidUpdate(prevPops, prevState) {
-      const { contacts} = this.state;
-      if (contacts !== prevState.contacts) {
-        console.log("Обновилось поле контактов");
-        localStorage.setItem('contacts', JSON.stringify(contacts));
-      }
-    }
-
-  
-  // addContact = ({ name, number }) => {
-  //   const { contacts } = this.state;
-  //   const contact = {
-  //     id: uuidv4(),
-  //     name,
-  //     number
-  //   };
-    
-  //   if (contacts.find(option => option.name.toLowerCase() === name.toLowerCase())) {
-  //     alert(`${name} is already in contacts`);
-  //     return;
-  //   }
-    
-  //   this.setState(({ contacts }) => ({
-  //     contacts: [contact, ...contacts]
-  //   }))
-  // }
-  
-  // deleteContact = contactId => {
-  //   this.setState(prevState => ({
-  //     contacts:prevState.contacts.filter(contact => contact.id !==contactId),
-  //   }))
-  // }
-
-  changeFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
-  }
-
-  // getVisibleContacts = () => {
-  //   const { images, filter } = this.state;
-  //   const normilizedFilter = filter.toLowerCase();
-  //   return images.filter(image =>
-  //     image.name.toLowerCase().includes(normilizedFilter));
-  // }
 
   toggleModal = () => {
     this.setState(({showModal}) => ({
@@ -99,13 +38,14 @@ class App extends Component {
   
   render() {
     const { searchName, showModal, images, loading } = this.state;
-    // const visibleContacts = this.getVisibleContacts();
+    
     return (
       <Container>
         <div>
           <Searchbar onSubmit={this.handleSearchSubmit} />
           <ToastContainer autoClose={3000}/>
           <ImageGallery searchName={searchName} />
+          
           <button type="button" onClick ={this.toggleModal}>
           Open modal
           </button>
@@ -115,13 +55,6 @@ class App extends Component {
             <button type="button" onClick={this.toggleModal}>Close Modal
             </button>
           </Modal>}
-          <div>
-            {/* проверяем: если что-то начинает грузиться- будет отрисовываться разметка... */}
-            {loading && <h1>Loading...</h1>}
-            {/* проверяем: если что-то есть в state в images - будет отрисовываться разметка... */}
-            {images && (<div>тут будут картинки</div>)}
-          </div>
-          {/* <Loader contacts={visibleContacts} /> */}
         </div>
       </Container>
     );
--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 import axios from "axios";
 //import s from './ContactList.module.css';
 import ImageGalleryItem from '../ImageGalleryItem';
+import SpinnerLoader from '../Loader';
+import { toast } from 'react-toastify';
+
 
 const baseUrl = "https://pixabay.com/api/";
 axios.defaults.baseURL = baseUrl;
@@ -11,43 +14,106 @@ const apiKey = "22651538-53630abe578d2561aeb41817a";
 export default class ImageGallery extends Component {
     state = {
         images: null,
-        loading:false
+        // loading: false,
+        error:null,
+        status: 'idle'
   }
 
     async componentDidUpdate(prevProps, prevState) {
-        this.setState({ loading: true });
+        
         const prevName = prevProps.searchName;
         const nextName = this.props.searchName;
 
         if (prevName !== nextName) {
-            console.log("изменилось имя покемона");
+            this.setState({ status: 'pending' });
             let queryParams = `?key=${apiKey}&q=${nextName}&image_type=photo&per_page=12&page=1&orientation=horizontal&safesearch=true`;
             let url = baseUrl + queryParams;
-            const response = await  axios.get(url);
-            const images = response.data.hits;
-            console.log(images);
-            this.setState({ images, loading: false});
-        }
+
+            //fetch
+               await fetch(url)
+                    .then(response => {
+                        if (response.ok) {
+                            return response.json();
+                        }
+                        return Promise.reject(
+                            new Error('Извините, по вашему запросу ничего не найдено'),
+                        );
+                    })
+                    .then(images =>
+                        this.setState({images: images.hits, status: 'resolved'}))
+                    .catch(error => this.setState({ error, status: 'rejected' }))
+            
+                // if (this.state.images.length === 0) {
+                //     // this.setState({ status: 'rejected' });
+                //     return toast.error('Извините, по вашему запросу ничего не найдено');
+                // }
+            };
+           
+            // if (this.state.images.length === 0) {
+            //     return toast.error('Извините, по вашему запросу ничего не найдено');
+            // }
+        
     }
+            
+            // axios  
+            // try {
+            //     const response = await axios.get(url);
+            //     const images = response.data.hits;
+            //     console.log(images);
+            //     this.setState({ images, status: 'resolved' });
+            // } catch (error) {
+            //     this.setState({ status: 'rejecting' });
+            //     throw error;
+            // }
+            // if (this.state.images.length === 0) {
+            //     return toast.error('Извините, по вашему запросу ничего не найдено');
+            // }
+        
     render() {
-        const { images, loading } = this.state;
-        const { searchName } = this.props;
-        return (
-            <div>
-                {loading && <h1>Loading...</h1>}
-                {!searchName && <div>Введите слово для поиска</div>}
-                {this.state.images &&
-                    <ul >
+        const { images, status } = this.state;
+
+        if (status === 'idle') {
+            return <div>Введите слово для поиска</div>
+        }
+
+        if (status === 'pending') {
+            <SpinnerLoader />
+        }
+
+        if (status === 'rejected') {
+            return toast.error('Извините, по вашему запросу ничего не найдено');
+        }
+
+        if (status === 'resolved') {
+            return (
+                <div>
+                    <ul className="GalleryList" >
                         {images.map(image=> (
-                            <li className="ImageGalleryItem" key={images.id}  >
-                                <ImageGalleryItem src={images.previewURL} alt={images.tags } />
+                            <li className="ImageGalleryItem" key={image.id}  >
+                                <ImageGalleryItem src={image.previewURL} alt={image.tags} />
                             </li>
                         ))}
-                    </ul>}
-                    <p>{searchName}</p>
+                    </ul>
             </div>
+            )
+        }
+
+        // return (
+        //     <div>
+        //         {loading && <h1>Loading...</h1>}
+        //         {!searchName && <div>Введите слово для поиска</div>}
+        //         {this.state.images &&
+        //             <ul className="GalleryList" >
+        //                 {images.map(image=> (
+        //                     <li className="ImageGalleryItem" key={image.id}  >
+        //                         <ImageGalleryItem src={image.previewURL} alt={image.tags} />
+        //                     </li>
+        //                 ))}
+        //             </ul>}
+        //             <p>{searchName}</p>
+        //     </div>
     
-        )
+        // )
     }
 }
 
