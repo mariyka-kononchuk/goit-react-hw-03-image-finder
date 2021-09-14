f96e14720724d8d053f81c435ab7f3f9164a6b25
Maria Kononchuk <77247240+mariyka-kononchuk@users.noreply.github.com>
2021-09-14 16:29:48 +0300

    Spinner style added

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import Loader from "react-loader-spinner";
+import {Spinner} from './Loader.styled.jsx'
 
 const SpinnerLoader = () => (
-    // <Loader
-    //     type="Puff"
-    //     color="#00BFFF"
-    //     height={100}
-    //     width={100}
-    //     timeout={3000} //3 secs
-    // />
-    <Loader className="Loader" type="BallTriangle" color="#00BFFF" height={80} width={80} />
+    <Spinner>
+        <Loader type="ThreeDots" color="#00BFFF" height={80} width={80} />
+    </Spinner>
 )
 
 export default SpinnerLoader;
